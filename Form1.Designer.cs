bbf5a83e5d095d2d89b68062a5831437f7d4e903
dnovak200 <dnovak200@gmail.com>
2024-03-20 22:18:43 +0100

    Dodan gornji panel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,46 +30,27 @@ namespace ZastitariTest
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            flowLayoutPanel1 = new FlowLayoutPanel();
-            panel2 = new Panel();
             button1 = new Button();
-            button3 = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button14 = new Button();
-            button25 = new Button();
-            flowLayoutPanel6 = new FlowLayoutPanel();
             flowLayoutPanel3 = new FlowLayoutPanel();
             button2 = new Button();
             button4 = new Button();
             button6 = new Button();
             button7 = new Button();
-            flowLayoutPanel1.SuspendLayout();
-            panel2.SuspendLayout();
-            flowLayoutPanel6.SuspendLayout();
+            flowLayoutPanel6 = new FlowLayoutPanel();
+            button13 = new Button();
+            button14 = new Button();
+            button12 = new Button();
+            button25 = new Button();
+            button3 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             flowLayoutPanel3.SuspendLayout();
+            flowLayoutPanel6.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.BackColor = Color.FromArgb(53, 92, 125);
-            flowLayoutPanel1.Controls.Add(panel2);
-            flowLayoutPanel1.Controls.Add(flowLayoutPanel3);
-            flowLayoutPanel1.Controls.Add(flowLayoutPanel6);
-            flowLayoutPanel1.Dock = DockStyle.Left;
-            flowLayoutPanel1.Location = new Point(0, 0);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(252, 940);
-            flowLayoutPanel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            panel2.Controls.Add(button1);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(3, 3);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(247, 57);
-            panel2.TabIndex = 3;
             // 
             // button1
             // 
@@ -80,7 +61,7 @@ namespace ZastitariTest
             button1.ForeColor = Color.Black;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(0, 0);
+            button1.Location = new Point(833, 406);
             button1.Name = "button1";
             button1.Padding = new Padding(10, 0, 0, 0);
             button1.Size = new Size(249, 57);
@@ -89,111 +70,6 @@ namespace ZastitariTest
             button1.TextAlign = ContentAlignment.MiddleLeft;
             button1.UseVisualStyleBackColor = false;
             // 
-            // button3
-            // 
-            button3.BackColor = Color.FromArgb(53, 92, 125);
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.Black;
-            button3.Image = (Image)resources.GetObject("button3.Image");
-            button3.ImageAlign = ContentAlignment.MiddleLeft;
-            button3.Location = new Point(833, 311);
-            button3.Name = "button3";
-            button3.Padding = new Padding(10, 0, 0, 0);
-            button3.Size = new Size(247, 57);
-            button3.TabIndex = 2;
-            button3.Text = "              Dodaj novi";
-            button3.TextAlign = ContentAlignment.MiddleLeft;
-            button3.UseVisualStyleBackColor = false;
-            // 
-            // button12
-            // 
-            button12.BackColor = Color.FromArgb(108, 91, 123);
-            button12.FlatAppearance.BorderSize = 0;
-            button12.FlatStyle = FlatStyle.Flat;
-            button12.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button12.ForeColor = Color.Black;
-            button12.Image = (Image)resources.GetObject("button12.Image");
-            button12.ImageAlign = ContentAlignment.MiddleLeft;
-            button12.Location = new Point(0, 120);
-            button12.Margin = new Padding(0);
-            button12.Name = "button12";
-            button12.Padding = new Padding(10, 0, 0, 0);
-            button12.Size = new Size(249, 57);
-            button12.TabIndex = 2;
-            button12.Text = "            Raspored";
-            button12.TextAlign = ContentAlignment.MiddleLeft;
-            button12.UseVisualStyleBackColor = false;
-            // 
-            // button13
-            // 
-            button13.BackColor = Color.FromArgb(53, 92, 125);
-            button13.FlatAppearance.BorderSize = 0;
-            button13.FlatStyle = FlatStyle.Flat;
-            button13.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button13.ForeColor = Color.Black;
-            button13.Image = (Image)resources.GetObject("button13.Image");
-            button13.ImageAlign = ContentAlignment.MiddleLeft;
-            button13.Location = new Point(3, 3);
-            button13.Name = "button13";
-            button13.Padding = new Padding(10, 0, 0, 0);
-            button13.Size = new Size(249, 57);
-            button13.TabIndex = 6;
-            button13.Text = "              Osobe";
-            button13.TextAlign = ContentAlignment.MiddleLeft;
-            button13.UseVisualStyleBackColor = false;
-            // 
-            // button14
-            // 
-            button14.BackColor = Color.FromArgb(108, 91, 123);
-            button14.FlatAppearance.BorderSize = 0;
-            button14.FlatStyle = FlatStyle.Flat;
-            button14.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button14.ForeColor = Color.Black;
-            button14.Image = (Image)resources.GetObject("button14.Image");
-            button14.ImageAlign = ContentAlignment.MiddleLeft;
-            button14.Location = new Point(0, 63);
-            button14.Margin = new Padding(0);
-            button14.Name = "button14";
-            button14.Padding = new Padding(10, 0, 0, 0);
-            button14.Size = new Size(249, 57);
-            button14.TabIndex = 2;
-            button14.Text = "            Dodaj novi";
-            button14.TextAlign = ContentAlignment.MiddleLeft;
-            button14.UseVisualStyleBackColor = false;
-            // 
-            // button25
-            // 
-            button25.BackColor = Color.FromArgb(108, 91, 123);
-            button25.FlatAppearance.BorderSize = 0;
-            button25.FlatStyle = FlatStyle.Flat;
-            button25.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button25.ForeColor = Color.Black;
-            button25.Image = (Image)resources.GetObject("button25.Image");
-            button25.ImageAlign = ContentAlignment.MiddleLeft;
-            button25.Location = new Point(0, 177);
-            button25.Margin = new Padding(0);
-            button25.Name = "button25";
-            button25.Padding = new Padding(10, 0, 0, 0);
-            button25.Size = new Size(249, 57);
-            button25.TabIndex = 2;
-            button25.Text = "            Export";
-            button25.TextAlign = ContentAlignment.MiddleLeft;
-            button25.UseVisualStyleBackColor = false;
-            // 
-            // flowLayoutPanel6
-            // 
-            flowLayoutPanel6.BackColor = Color.FromArgb(53, 92, 125);
-            flowLayoutPanel6.Controls.Add(button13);
-            flowLayoutPanel6.Controls.Add(button14);
-            flowLayoutPanel6.Controls.Add(button12);
-            flowLayoutPanel6.Controls.Add(button25);
-            flowLayoutPanel6.Location = new Point(3, 307);
-            flowLayoutPanel6.Name = "flowLayoutPanel6";
-            flowLayoutPanel6.Size = new Size(250, 235);
-            flowLayoutPanel6.TabIndex = 14;
-            // 
             // flowLayoutPanel3
             // 
             flowLayoutPanel3.BackColor = Color.FromArgb(53, 92, 125);
@@ -201,8 +77,7 @@ namespace ZastitariTest
             flowLayoutPanel3.Controls.Add(button4);
             flowLayoutPanel3.Controls.Add(button6);
             flowLayoutPanel3.Controls.Add(button7);
-            flowLayoutPanel3.Dock = DockStyle.Top;
-            flowLayoutPanel3.Location = new Point(3, 66);
+            flowLayoutPanel3.Location = new Point(528, 51);
             flowLayoutPanel3.Name = "flowLayoutPanel3";
             flowLayoutPanel3.Size = new Size(250, 235);
             flowLayoutPanel3.TabIndex = 18;
@@ -282,28 +157,179 @@ namespace ZastitariTest
             button7.TextAlign = ContentAlignment.MiddleLeft;
             button7.UseVisualStyleBackColor = false;
             // 
+            // flowLayoutPanel6
+            // 
+            flowLayoutPanel6.BackColor = Color.FromArgb(53, 92, 125);
+            flowLayoutPanel6.Controls.Add(button13);
+            flowLayoutPanel6.Controls.Add(button14);
+            flowLayoutPanel6.Controls.Add(button12);
+            flowLayoutPanel6.Controls.Add(button25);
+            flowLayoutPanel6.Location = new Point(876, 54);
+            flowLayoutPanel6.Name = "flowLayoutPanel6";
+            flowLayoutPanel6.Size = new Size(250, 235);
+            flowLayoutPanel6.TabIndex = 14;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.FromArgb(53, 92, 125);
+            button13.FlatAppearance.BorderSize = 0;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.ForeColor = Color.Black;
+            button13.Image = (Image)resources.GetObject("button13.Image");
+            button13.ImageAlign = ContentAlignment.MiddleLeft;
+            button13.Location = new Point(3, 3);
+            button13.Name = "button13";
+            button13.Padding = new Padding(10, 0, 0, 0);
+            button13.Size = new Size(249, 57);
+            button13.TabIndex = 6;
+            button13.Text = "              Osobe";
+            button13.TextAlign = ContentAlignment.MiddleLeft;
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.FromArgb(108, 91, 123);
+            button14.FlatAppearance.BorderSize = 0;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button14.ForeColor = Color.Black;
+            button14.Image = (Image)resources.GetObject("button14.Image");
+            button14.ImageAlign = ContentAlignment.MiddleLeft;
+            button14.Location = new Point(0, 63);
+            button14.Margin = new Padding(0);
+            button14.Name = "button14";
+            button14.Padding = new Padding(10, 0, 0, 0);
+            button14.Size = new Size(249, 57);
+            button14.TabIndex = 2;
+            button14.Text = "            Dodaj novi";
+            button14.TextAlign = ContentAlignment.MiddleLeft;
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(108, 91, 123);
+            button12.FlatAppearance.BorderSize = 0;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.Black;
+            button12.Image = (Image)resources.GetObject("button12.Image");
+            button12.ImageAlign = ContentAlignment.MiddleLeft;
+            button12.Location = new Point(0, 120);
+            button12.Margin = new Padding(0);
+            button12.Name = "button12";
+            button12.Padding = new Padding(10, 0, 0, 0);
+            button12.Size = new Size(249, 57);
+            button12.TabIndex = 2;
+            button12.Text = "            Raspored";
+            button12.TextAlign = ContentAlignment.MiddleLeft;
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button25
+            // 
+            button25.BackColor = Color.FromArgb(108, 91, 123);
+            button25.FlatAppearance.BorderSize = 0;
+            button25.FlatStyle = FlatStyle.Flat;
+            button25.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button25.ForeColor = Color.Black;
+            button25.Image = (Image)resources.GetObject("button25.Image");
+            button25.ImageAlign = ContentAlignment.MiddleLeft;
+            button25.Location = new Point(0, 177);
+            button25.Margin = new Padding(0);
+            button25.Name = "button25";
+            button25.Padding = new Padding(10, 0, 0, 0);
+            button25.Size = new Size(249, 57);
+            button25.TabIndex = 2;
+            button25.Text = "            Export";
+            button25.TextAlign = ContentAlignment.MiddleLeft;
+            button25.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(53, 92, 125);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Black;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(833, 311);
+            button3.Name = "button3";
+            button3.Padding = new Padding(10, 0, 0, 0);
+            button3.Size = new Size(247, 57);
+            button3.TabIndex = 2;
+            button3.Text = "              Dodaj novi";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(53, 92, 125);
+            panel1.BackgroundImageLayout = ImageLayout.None;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1471, 45);
+            panel1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 10.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(64, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 20);
+            label1.TabIndex = 21;
+            label1.Text = "InTouch Soft Design";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(45, 37);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 20;
+            pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(53, 92, 125);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 45);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(250, 895);
+            flowLayoutPanel1.TabIndex = 20;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1471, 940);
-            Controls.Add(button3);
             Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Controls.Add(flowLayoutPanel6);
+            Controls.Add(flowLayoutPanel3);
             Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ForeColor = Color.White;
             Name = "Form1";
             Text = "Form1";
-            flowLayoutPanel1.ResumeLayout(false);
-            panel2.ResumeLayout(false);
-            flowLayoutPanel6.ResumeLayout(false);
             flowLayoutPanel3.ResumeLayout(false);
+            flowLayoutPanel6.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private FlowLayoutPanel flowLayoutPanel1;
         private Button button1;
-        private Panel panel2;
         private Button button3;
         private Button button12;
         private Button button13;
@@ -315,5 +341,9 @@ namespace ZastitariTest
         private Button button4;
         private Button button6;
         private Button button7;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
